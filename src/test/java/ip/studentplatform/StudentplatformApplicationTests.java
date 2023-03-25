967f93480cc8cdeb1ec51ef2bf202fa2e2cbcb28
Eduard-Costin <eduard_costin2001@yahoo.com>
2023-03-25 09:45:48 +0200

    start project

---
+++ src/test/java/ip/studentplatform/StudentplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ip.studentplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
