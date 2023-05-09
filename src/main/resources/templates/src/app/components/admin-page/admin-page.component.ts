f37e598f65eb8382ae0d57c3cdb1005f12dfdd50
victor1606 <victor.ion42@gmail.com>
2023-05-09 23:47:33 +0300

    getMaterie
    Signed-off-by: victor1606 <victor.ion42@gmail.com>

--- src/main/resources/templates/src/app/components/admin-page/admin-page.component.ts
+++ src/main/resources/templates/src/app/components/admin-page/admin-page.component.ts
@@ -1,50 +1,64 @@
-import {Component, NgModule, Renderer2, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {FormBuilder, FormGroup, FormsModule} from '@angular/forms';
-import {BrowserModule} from "@angular/platform-browser";
+import {FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-admin-page',
-  templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.css']
+    selector: 'app-admin-page',
+    templateUrl: './admin-page.component.html',
+    styleUrls: ['./admin-page.component.css']
 })
-
 
 
 export class AdminPageComponent {
 
-  // constructor(private http: HttpClient) {}
+    strings: string[] = [];
+    // form: FormGroup;
 
-  form: FormGroup;
-  constructor(public fb: FormBuilder, private http: HttpClient) {
-    this.form = this.fb.group({
-      name: [''],
-      avatar: [null],
-    });
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  ngOnInit() {}
+    // var express = require('express')
+    // var cors = require('cors')
+    // var app = express()
+    //
+    // app.use(cors())
 
-  uploadCustomersData(fileInput: any) {
-    const file  = fileInput.files[0];
-    const formData = new FormData();
-    formData.append('file', file, file.name);
+    ngOnInit() {
+        this.http.get<string[]>('http://localhost:8082/class/getMaterie').subscribe(data => {
+            this.strings = data;
+            console.log(this.strings);
+        });
+    }
 
-    this.http.post('http://localhost:8082/user/upload-customers-data', formData).subscribe(response => {
-      console.log(response);
-    });
-  }
+    // constructor(public fb: FormBuilder, private http: HttpClient) {
+    //     this.form = this.fb.group({
+    //         name: [''],
+    //         avatar: [null],
+    //     });
+    // }
 
-  // manualEnroll() {
-  //   const formData: any = new FormData();
-  //   formData.append('name', this.form.get('name').value);
-  //   formData.append('avatar', this.form.get('avatar').value);
-  //   this.http
-  //     .post('http://localhost:8082/class/addStudent', formData)
-  //     .subscribe({
-  //       next: (response) => console.log(response),
-  //       error: (error) => console.log(error),
-  //     });
-  // }
+
+    // uploadCustomersData(fileInput: any) {
+    //     const file = fileInput.files[0];
+    //     const formData = new FormData();
+    //     formData.append('file', file, file.name);
+    //
+    //     this.http.post('http://localhost:8082/user/upload-customers-data', formData).subscribe(response => {
+    //         console.log(response);
+    //     });
+    // }
+
+
+    // manualEnroll() {
+    //   const formData: any = new FormData();
+    //   formData.append('name', this.form.get('name').value);
+    //   formData.append('avatar', this.form.get('avatar').value);
+    //   this.http
+    //     .post('http://localhost:8082/class/addStudent', formData)
+    //     .subscribe({
+    //       next: (response) => console.log(response),
+    //       error: (error) => console.log(error),
+    //     });
+    // }
 
 }
