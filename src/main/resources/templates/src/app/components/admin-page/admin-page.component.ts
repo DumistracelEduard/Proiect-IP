331dff5a51417e5bbd7b766801cc4ee33f0fb29b
victor1606 <victor.ion42@gmail.com>
2023-05-31 15:32:03 +0300

    approveAll
    Signed-off-by: victor1606 <victor.ion42@gmail.com>

--- src/main/resources/templates/src/app/components/admin-page/admin-page.component.ts
+++ src/main/resources/templates/src/app/components/admin-page/admin-page.component.ts
@@ -80,6 +80,29 @@ export class AdminPageComponent {
         });
     }
 
+    approveAll() {
+        let i = 0;
+        for (i = 0; i < this.grades.length; ++i) {
+            const formData = new FormData();
+            formData.append("firstName", this.grades[i].firstName);
+            formData.append("lastName", this.grades[i].lastName);
+            formData.append("materie", this.selectedSubject);
+
+            this.http.put("http://localhost:8082/Grades/approvedGrades", formData).subscribe({
+                next: (response) => {
+                    console.log(response);
+
+                },
+                error: (error) => {
+                    console.log(error);
+                    this.showSnackbar('EROARE: Aprobarea nu a fost realizată cu succes.', 'Ok', 5000);
+                },
+            })
+
+            this.showSnackbar('Toate notele sunt aprobate.', 'Ok', 5000);
+        }
+    }
+
     approveGrade(firstname: string, lastname: string) {
         const formData = new FormData();
         formData.append("firstName", firstname);
@@ -101,7 +124,6 @@ export class AdminPageComponent {
     ngOnInit() {
         this.http.get<string[]>('http://localhost:8082/class/getMaterie').subscribe(data => {
             this.strings = data;
-            // console.log(this.strings);
         });
     }
 
@@ -129,8 +151,6 @@ export class AdminPageComponent {
         const username = this.form.get('username')!.value;
         const role = this.form.get('role')!.value;
         const subject = this.form.get('subject')!.value;
-
-        // console.log(username + " " + role + " " + subject);
 
         if (username != "" && subject != "") {
             formData.append('username', username);
